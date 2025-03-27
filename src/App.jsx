6fa49986df5b2e9829559a7ba51e49c2f80d677d
Jovana Marceta <jmarcetafedev@gmail.com>
2025-03-27 17:01:42 +0100

    Adding single job page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
+import JobPage from "./pages/JobPage";
 import MainLayout from "./layouts/MainLayout";
 import NotFoundPage from "./pages/NotFoundPage";
 
@@ -14,6 +15,7 @@ const router = createBrowserRouter(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />} />
+      <Route path="/jobs/:id" element={<JobPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Route>
   )
---
+++ src/pages/JobPage.jsx
@@ -0,0 +1,10 @@
+const JobPage = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+     
+    </div>
+  )
+}
+
+export default JobPage
