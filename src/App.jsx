a6a87a2656bf79ea086b52f86e62ca0996d937f3
Jovana Marceta <jmarcetafedev@gmail.com>
2025-03-27 15:43:38 +0100

    Adding routing and server

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom';
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import JobListings from "./components/JobListings";
-import ViewAllJobs from "./components/ViewAllJobs";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
+import MainLayout from "./layouts/MainLayout";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route index element={ <h1> my app</h1>} />)
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Route>
+  )
 );
 
 const App = () => {
   return <RouterProvider router={router} />;
-}
+};
 
-export default App
+export default App;
