17007eeda7d5b36d66f8fbb7a3aeec68fa986375
Jovana Marceta <jmarcetafedev@gmail.com>
2025-03-31 13:50:14 +0200

    get data for job page with use effect

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,10 +1,29 @@
-const JobPage = () => {
-  return (
-    <div>
-      <h1>hello</h1>
-     
-    </div>
-  )
-}
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
 
-export default JobPage
+const JobPage = () => {
+  const { id } = useParams();
+  const [job, setJob] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJob = async () => {
+      try {
+        const res = await fetch(`/api/jobs/${id}`);
+        const data = await res.json();
+        setJob(data);
+      } catch (error) {
+        console.log("Error fetching data", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchJob();
+  }, []);
+
+  return loading ? <Spinner /> : <h1>{job.title}</h1>;
+};
+
+export default JobPage;
